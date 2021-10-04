db152197852f403557256c8ad6e1b1933daca092
Eduardo <ealvarez@carminaticonsulting.com>
2021-10-04 08:57:51 -0500

    first lesson

--- tutorial/src/App.js
+++ tutorial/src/App.js
@@ -1,17 +1,27 @@
 import './App.css';
 
 function App() {
-  let test1 = "Testing This let variable"
-  const test2 = 20
-  test1 = 30
+  let stringVariable = "Testing old school functions";
+  const test2 = 20;
+  let numberVariable = 100;
+  let booleanVariable = true;
+                        //  0         1     2     3
+  let arrayVariable = ["javascript", 30, true, Date()];
 
+//functon declaration - function name
+  function oldSchoolFunction() {
+    let oldSchool = "Old School variable"
+    return oldSchool;
+  }
+                        
+  const arrowFunction = (a,b,c) => {
+    return a + b + c
+  }
 
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          {test1}
-        </p>
+        <p>{arrowFunction("string1","string2", "string3")}</p>
       </header>
     </div>
   );
